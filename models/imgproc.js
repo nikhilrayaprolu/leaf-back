9c3d5a50f031f804795738286ffda0a6a60044a3
Benedict Florance <benedictflorance.a@gmail.com>
2018-05-31 15:21:51 +0530

    TaggingComplete bug

--- models/imgproc.js
+++ models/imgproc.js
@@ -16,18 +16,6 @@ module.exports = {
                 addExif.addExif(file.filename, exifData);
                 console.log(exifData);
             });
-            Jimp.read(file.destination + file.filename, function (err, lenna) {
-                console.log(lenna);
-                if (err) throw err;
-                if(lenna.bitmap.height>lenna.bitmap.width)
-                    lenna.rotate(90);
-                lenna
-                    .write(file.destination + file.filename + '.jpg', function (err,data) {
-                        console.log(err,data);
-                    }); // save
-                console.log(file.destination + file.filename);
-                fs.unlink(file.destination+file.filename);
-            });
             });
     }
 };
