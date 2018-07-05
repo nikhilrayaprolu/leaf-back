588bcfd3e41ac22d5c4cbc00c0e6f7c7a17a6300
Benedict Florance <benedictflorance.a@gmail.com>
2018-07-05 23:50:07 +0530

    Change URL to preon

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://preon.iiit.ac.in');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
